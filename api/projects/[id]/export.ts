d5d07c4864620a6dd2d8baceabaff13f9e9132b3
nuspat83-ux <nuspat83@gmail.com>
2026-09-19 07:54:32 -0700

    fix: route project export function

---
+++ api/projects/[id]/export.ts
@@ -0,0 +1,7 @@
+import type { IncomingMessage, ServerResponse } from 'node:http';
+import { waitUntil } from '@vercel/functions';
+import { requestHandler } from '../../../../server/index.js';
+
+export default function handler(req: IncomingMessage, res: ServerResponse) {
+  return requestHandler(req, res, waitUntil);
+}
