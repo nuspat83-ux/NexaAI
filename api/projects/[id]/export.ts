b72e89b8db0d1bef383be38ff80e3578456038cc
nuspat83-ux <nuspat83@gmail.com>
2026-09-19 07:55:34 -0700

    fix: correct production project route import

--- api/projects/[id]/export.ts
+++ api/projects/[id]/export.ts
@@ -1,6 +1,6 @@
 import type { IncomingMessage, ServerResponse } from 'node:http';
 import { waitUntil } from '@vercel/functions';
-import { requestHandler } from '../../../../server/index.js';
+import { requestHandler } from '../../../server/index.js';
 
 export default function handler(req: IncomingMessage, res: ServerResponse) {
   return requestHandler(req, res, waitUntil);
